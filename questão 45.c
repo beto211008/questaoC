9a46a45ff7e5e177b6d052d95a81d765b3ec28e1
João Roberto <164904601+beto211008@users.noreply.github.com>
2025-06-10 21:42:31 -0300

    Create questão 45.c

---
+++ questão 45.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void){
+float num1,num2; char operador;
+    printf("Digite dois números para ser umaa operação: ");
+    scanf("%f %f",&num1 ,&num2);
+    printf("\nDigíte um símbolo de operação desejada(+, -, /,*): ");
+  scanf("%s", &operador);
+  
+switch(operador){
+  case '+':
+    printf("\nA soma de %1.1f + %1.1f é:%1.1f ",num1,num2 ,num1 + num2 );
+  break;
+  case '-':
+    printf("\nA Subtração de %1.1f - %1.1f é:%1.1f ",num1,num2 ,num1 - num2 );
+  break;
+  case '*':
+  printf("\nA multiplicação de %1.1f X %1.1f é:%1.1f ",num1,num2 ,num1 * num2 );
+  break;
+  case '/':
+  printf("\nA divisão de %1.1f / %1.1f é:%1.1f ",num1,num2 ,num1 / num2 );
+  break;
+  default:
+  printf("\n Digite um símbolo válido.\n " );
+  break;
+};
+return 0;}
