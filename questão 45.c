41068b125130db379daa756c1a91aa35d2369268
João Roberto <164904601+beto211008@users.noreply.github.com>
2025-06-12 08:20:49 -0300

    Update questão 45.c

--- questão 45.c
+++ questão 45.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void){
 float num1,num2; char operador;
-    printf("Digite dois números para ser umaa operação: ");
-    scanf("%f %f",&num1 ,&num2);
-    printf("\nDigíte um símbolo de operação desejada(+, -, /,*): ");
-  scanf("%s", &operador);
-  
+  do {
+    printf("Digite dois números para ser feita uma operação: ");
+      scanf("%f %f",&num1 ,&num2);
+      printf("\nDigíte um símbolo de operação desejada(+, -, /,*): ");
+    scanf("%s", &operador);
 switch(operador){
   case '+':
     printf("\nA soma de %1.1f + %1.1f é:%1.1f ",num1,num2 ,num1 + num2 );
@@ -22,5 +22,5 @@ switch(operador){
   default:
   printf("\n Digite um símbolo válido.\n " );
   break;
-};
+};}while((operador!='+') && (operador!='-' && (operador!='*') && (operador!='/')));
 return 0;}
